569bd2f3472c01d68ae5dbd1a5b887374cb69ba8
Victor Prieto Sutil <vprieto-@c3r11s2.42madrid.com>
2022-09-23 19:45:44 +0200

    fajnan

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: vprieto- <vprieto-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/17 17:00:33 by vprieto-          #+#    #+#             */
-/*   Updated: 2022/09/23 19:21:03 by vprieto-         ###   ########.fr       */
+/*   Updated: 2022/09/23 19:41:56 by vprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,24 +20,22 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	i = 0;
 	if (start >= ft_strlen(s))
 	{
-		str = calloc(1, 1);
+		str = ft_calloc(1, sizeof(char));
 		return (str);
 	}
-	else if (len> ft_strlen(s) - start)
-		str = malloc( ft_strlen(s) - start + 1);
+	else if (len + start > ft_strlen(s))
+		str = malloc(sizeof(char) * (ft_strlen(s) - ((int)start) + 1));
 	else
-	str = malloc(len + 1);
+		str = malloc(sizeof(char) * (len + 1));
 	if (str == NULL)
 		return (NULL);
 	i = 0;
 	while ((i < len) && s[start] != 0)
-	{
 		str[i++] = s[start++];
-	}
 	str[i] = '\0';
 	return (str);
 }
-
+/*
 #include <string.h>
 int main()
 {
@@ -53,4 +51,4 @@ int main()
 	//printf("%s", ret2);
 
 }
-
+*/
